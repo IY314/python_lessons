d0b195f3fdb515cdad200cd41493d1f29935f7ea
Isaac Yee <isaac.cl.yee@gmail.com>
2019-04-20 14:24:24 -0700

    first commit

---
+++ lesson1.py
@@ -0,0 +1,20 @@
+def is_integer(num):
+    if int(num) != num:
+        return False
+    else:
+        return True
+
+
+
+def prime(x):
+    for factor in range(2, x):
+        div = x / factor
+        if is_integer(div):
+            print('It is composite; can be divided by %d'%factor)
+            return
+    print('It is prime')
+
+
+
+prime(1001)
+    
